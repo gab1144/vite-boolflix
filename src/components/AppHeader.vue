c7f137f5e7b7540098f726f9868f5cff2bd9084e
gab1144 <gabry.rinciari04@gmail.com>
2022-11-21 12:05:52 +0100

    [add] struttura

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+  export default{
+    name: 'AppHeader'
+  }
+</script>
+
+<template>
+  <header>
+    <h1>BOOLFLIX</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+  @use '../styles/partials/vars' as *;
+  @use '../styles/partials/mixin' as *;
+
+  header{
+    background-color: $header-background-color;
+    height: 80px;
+    padding: 0 20px;
+    @include vCenterSpcBtw();
+    h1 {
+      color: red;
+    }
+  }
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+  export default{
+    name: 'AppMain'
+  }
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h2>main</h2>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  @use '../styles/partials/vars' as *;
+  @use '../styles/partials/mixin' as *;
+
+  main {
+    background-color: $main-background-color;
+    color: white;
+  }
+</style>
